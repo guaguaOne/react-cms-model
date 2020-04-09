6a3393b20619f0a2758a999628f58a67d1e1fed1
chengnian <chengnian@xinhuamm.net>
2020-04-09 13:11:12 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+import RouterSetting from '@/config/routerSetting';
+function App() {
+  return (
+    <RouterSetting />
+  );
+}
+
+export default App;
